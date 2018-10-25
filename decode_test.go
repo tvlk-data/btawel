3a2773e61c6f484cfb4cfaebebef7a71e61a270a
Imre Nagi <imre.nagi2812@gmail.com>
2018-10-25 19:23:39 +0700

    Implement decoder for pointer

--- decode_test.go
+++ decode_test.go
@@ -371,3 +371,202 @@ func TestReadColumnQualifiers(t *testing.T) {
 	require.Equal(t, "test", cqs[0])
 	require.Equal(t, "test2", cqs[1])
 }
+
+func TestReadPointerReference(t *testing.T) {
+
+	buf := &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, int64(23))
+
+	row := map[string][]bigtable.ReadItem{
+		"info": []bigtable.ReadItem{
+			bigtable.ReadItem{
+				Row:    "john",
+				Column: "info:name",
+				Value:  []byte("John"),
+			},
+			bigtable.ReadItem{
+				Row:    "john",
+				Column: "info:age",
+				Value:  buf.Bytes(),
+			},
+		},
+	}
+
+	var person struct {
+		Name    *string `bigtable:"info:name"`
+		Address *string `bigtable:"info:address"`
+		Age     *int    `bigtable:"info:age"`
+	}
+	err := btawel.ReadRow(row, &person)
+
+	require.NoError(t, err)
+	require.Equal(t, "John", *(person.Name))
+	require.Equal(t, int(23), *(person.Age))
+	require.Nil(t,  person.Address)
+}
+
+
+func TestReadPointer(t *testing.T) {
+
+	s := struct {
+		TNonTag  *string
+		TRowKey  *string  `bigtable:",rowkey"`
+		TString  *string  `bigtable:"fc:tstr"`
+		TBool    *bool    `bigtable:"fc:tbool"`
+		TInt     *int     `bigtable:"fc:tint"`
+		TInt8    *int8    `bigtable:"fc:tint8"`
+		TInt16   *int16   `bigtable:"fc:tint16"`
+		TInt32   *int32   `bigtable:"fc:tint32"`
+		TInt64   *int64   `bigtable:"fc:tint64"`
+		TUint    *uint    `bigtable:"fc:tuint"`
+		TUint8   *uint8   `bigtable:"fc:tuint8"`
+		TUint16  *uint16  `bigtable:"fc:tuint16"`
+		TUint32  *uint32  `bigtable:"fc:tuint32"`
+		TUint64  *uint64  `bigtable:"fc:tuint64"`
+		TFloat32 *float32 `bigtable:"fc:tfloat32"`
+		TFloat64 *float64 `bigtable:"fc:tfloat64"`
+	}{}
+
+	key := "thisisrowkey"
+	bstr := "bytebyte"
+	str := "hoge"
+	bl := true
+	num := 123
+	buf := &bytes.Buffer{}
+
+	ris := []bigtable.ReadItem{
+		bigtable.ReadItem{
+			Row:    key,
+			Column: "fc:tbytes",
+			Value:  []byte(bstr),
+		},
+		bigtable.ReadItem{
+			Row:    key,
+			Column: "fc:tstr",
+			Value:  []byte(str),
+		},
+		bigtable.ReadItem{
+			Row:    key,
+			Column: "fc:tbool",
+			Value:  boolconv.NewBool(bl).Bytes(),
+		},
+	}
+
+	binary.Write(buf, binary.BigEndian, int64(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tint",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, int8(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tint8",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, int16(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tint16",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, int32(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tint32",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, int64(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tint64",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, uint64(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tuint",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, uint8(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tuint8",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, uint16(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tuint16",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, uint32(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tuint32",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, uint64(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tuint64",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, float32(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tfloat32",
+		Value:  buf.Bytes(),
+	})
+
+	buf = &bytes.Buffer{}
+	binary.Write(buf, binary.BigEndian, float64(num))
+	ris = append(ris, bigtable.ReadItem{
+		Row:    key,
+		Column: "fc:tfloat64",
+		Value:  buf.Bytes(),
+	})
+
+	t.Run("ReadRow should parse data correctly", func(t *testing.T) {
+		row := bigtable.Row{"fc": ris}
+		err := btawel.ReadRow(row, &s)
+
+		require.NoError(t, err)
+		require.Equal(t, key, *s.TRowKey)
+
+		require.Equal(t, str, *s.TString)
+		require.True(t, *s.TBool)
+		require.Equal(t, int(num), *s.TInt)
+		require.Equal(t, int8(num), *s.TInt8)
+		require.Equal(t, int16(num), *s.TInt16)
+		require.Equal(t, int32(num), *s.TInt32)
+		require.Equal(t, int64(num), *s.TInt64)
+		require.Equal(t, uint(num), *s.TUint)
+		require.Equal(t, uint8(num), *s.TUint8)
+		require.Equal(t, uint16(num), *s.TUint16)
+		require.Equal(t, uint32(num), *s.TUint32)
+		require.Equal(t, uint64(num), *s.TUint64)
+		require.Equal(t, float32(num), *s.TFloat32)
+		require.Equal(t, float64(num), *s.TFloat64)
+	})
+}
--- go.mod
+++ go.mod
@@ -2,11 +2,14 @@ module github.com/tvlk-data/btawel
 
 require (
 	cloud.google.com/go v0.28.0
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fatih/structs v1.0.0
 	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
 	github.com/google/go-cmp v0.2.0 // indirect
 	github.com/googleapis/gax-go v2.0.0+incompatible // indirect
 	github.com/osamingo/boolconv v0.0.0-20151016060535-9ef56333404f
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.2.2
 	go.opencensus.io v0.17.0 // indirect
 	golang.org/x/net v0.0.0-20180926154720-4dfa2610cdf3 // indirect
 	google.golang.org/api v0.0.0-20180929000454-5da02d31af7d // indirect
